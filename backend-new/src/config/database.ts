9e495cfe4ac1c7cac843d1c34ad777d8b00814c1
Muhammad Nauman <numan98khan@gmail.com>
2025-12-12 19:09:36 +0300

    updated mongo

--- backend-new/src/config/database.ts
+++ backend-new/src/config/database.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/instagram-inbox');
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://mongo:uXSQuEFJSoZaBeALSbQnqcnnEbGESHiI@interchange.proxy.rlwy.net:34650');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error}`);
